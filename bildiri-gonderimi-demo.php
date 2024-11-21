90a90fcd6d8d293729759943f9ab729a17387ae9
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2024-11-21 12:54:27 +0300

    bildiri gönderimi için demo sayfa hazırlandı.

--- bildiri-gonderimi-demo.php
+++ bildiri-gonderimi-demo.php
@@ -20,7 +20,7 @@
 		<?php include("header.php") ?>
 		<!-- Blog Page -->
 		<!-- Blog Single Page  -->
-		<div id="page-section">
+		<div id="page-section" class="abstract">
 			<section id="blog">
 				<div class="container-fluid">
 					<div class="jumbotron"></div><!-- /jumbotron-->
@@ -32,122 +32,62 @@
 			</section><!--Section Blog ends -->
 			<!-- Section Services -->
 			<section class="container" id="services">
-				<!-- Section heading -->
-				<div class="col-lg-6">
-					<div class="section-heading">
-						<h2 class="mt-5">BİLDİRİ GÖNDERİMİ</h2><!-- divider -->
-						<div class="hr"></div>
-					</div>
-					<div class="row" style="padding-bottom:30px; display:flex; justify-content:center;">
-						<!-- Services Boxes -->
-						<a href="https://solo.digiabstract.com/caaad2023/" target="_blank">
-							<div class="serviceBox col-lg-12 bg-color1" style="margin-top:0;">
-								<!-- service image-->
-								<div class="service-content text-light">
-									<h4 class="title" style="font-size:23px; padding:0px; margin-bottom:0px; margin-top:0px;">Online Bildiri Gönderme Sistemi</h4>
-								</div>
-							</div>
-						</a>
-						<!-- /service box -->
-					</div>
-					<div>
-						<h4>Bildiri Gönderim Kuralları</h4>
-						<!-- Custom List -->
-						<ul class="custom no-margin" style="font-weight:normal;">
-							<li>Sunum Dili Türkçe olacaktır.</li>
-							<li>Bildiriniz özgün olmalıdır.</li>
-							<li>Bildiriniz 350 kelimeyi geçmemelidir.</li>
-							<li>Bildiri gönderimi son bildiri gönderim tarihini geçmemelidir. Son bildiri tarihinden sonra gönderilen bildiriler kabul edilmeyecektir.</li>
-							<li>Bildiri gönderilmeden önce bildiri sahibi ile bildiride yer alan yazarlar bildirinin son hali için emin olmalıdır, ardından bildiri sistemine yüklenmelidir.</li>
-							<li>Sunum yapacak kişinin katılımcı olarak kayıt yaptırması gerekmektedir.</li>
-							<li>Bildiriler sadece online gönderim modülü ile gönderilebilecektir. E-posta, faks vb. usullerle iletilen bildiriler değerlendirmeye alınmayacaktır.</li>
-							<li>Bildirileriniz için son gönderim tarihi <b>31 Mart 2023</b>, saat <b>23:59</b>’dur.</li>
-							<li>Bildiri değerlendirme sonuçları <b>4 Nisan 2023</b> tarihinde bildirilecektir.</li>
-							<li>Bildirisi kabul edilen kişiler için son kayıt tarihi <b>7 Nisan 2023</b>’tür.</li>
-						</ul>
-					</div>
-					<div>
-						<h4>Sunum Hazırlama</h4>
-						<!-- Custom List -->
-						<ul class="custom no-margin" style="font-weight:normal;">
-							<li>Başlamadan önce, lütfen aşağıdaki adımları izleyiniz:</li>
-							<li>Sunumu gerçekleştirecek kişinin iletişim bilgilerini eksiksiz yazmanız gerekmektedir.
-								<ul>
-									<li>E-posta adresi</li>
-									<li>Adres bilgileri</li>
-									<li>Gün içerisinde ve akşam ulaşılabilecek telefon numaraları</li>
-								</ul>
-							</li>
-							<li>Yazar ve diğer yazarların bilgileri detaylı olarak yer almalıdır.
-								<ul>
-									<li>Tam olarak isim ve soyadı bilgileri</li>
-									<li>Yazar isimlerinin ilk harfleri büyük diğerleri küçük olmalıdır.</li>
-									<li>Kurum bilginiz; bölüm, enstitü / hastane, şehir, ülke şeklinde olmalıdır.</li>
-								</ul>
-							</li>
-							<li>Bildiri başlığınız 30 kelimeyle sınırlı olmalı ve BÜYÜK HARFLER kullanılmalıdır.</li>
-							<li>Bildiriniz “Teşekkür” dahil olmak üzere 350 kelimeden oluşmalıdır.</li>
-						</ul>
-						<p><b>NOT:</b> Yazınızın düzenlenmesinde, edit edilmesinde ve kelime hesaplamasında Office programları kullanmanızı öneririz. (Örnek: Word kelime işlemcisi vb.)</p>
-						<ul class="custom no-margin">
-							<li>Yalnızca standart kısaltmalar kullanın. Özel bir yer ya da beklenmedik kısaltmalar parantez içerisinde açıklamalarıyla belirtilmelidir.</li>
-							<li>İlaç isimleri markalarıyla belirtilmelidir. Sayılarını adet olarak açıklayınız.</li>
-						</ul>
-						<p><b>Açıklama</b><br>Yazarın finansal bağlantısı var ise belirtmesi gerekir. Herhangi bir reklam ürününün üreticisi/tedarikçi ya da işiyle ilişkili bir hizmet sunu formunda uygun bir kutu içerisinde belirtilmelidir.</p>
 
-						<p><b>E-posterin Hazırlanması</b><br>Kongremizde Bildiri Değerlendirme kurulu tarafından kabul edilen posterler elektronik ortamda sergilenecektir.</p>
-
-						<p><b>Slayt Sayısı</b><br>Posterler elektronik ortamda sergilenecek olup yalnızca 1 slayt kullanılması gerekmektedir.</p>
-
-						<p><b>Slayt Boyutu</b><br>Kullanacağınız slayt, ‘‘Geniş Ekran’’(wide screen) olarak bilinen 16:9 boyutunda olmalıdır.</p>
-
-						<p><b>Slayt Arka Fonu</b><br>Posterinizin arka fonunda kullanmak istediğiniz bir görsel var ise 1920 x1080 piksel boyutta olması önerilir.</p>
-
-						<p><b>Başlık ve Logolar</b><br>Koyu puntoyla yazılmalı ve okuyucunun dikkatini çekecek bir başlık kullanılmalıdır.</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="section-heading">
-						<h2>KONGRE BURS ŞARTLARI</h2><!-- divider -->
-						<div class="hr"></div>
-					</div>
-					<div class="row" style="padding-bottom:30px; display:flex; justify-content:center;">
-						<!-- Services Boxes -->
-						<a href="doc/caaad-kongresi-bursu-basvuru-formu.docx" target="_blank">
-							<div class="serviceBox col-lg-12 bg-color1" style="margin-top:0;">
-								<!-- service image-->
-								<div class="service-content text-light">
-									<h4 class="title" style="font-size:23px; padding:0px; margin-bottom:0px; margin-top:0px;">Burs Başvuru Formu</h4>
-								</div>
-							</div>
-						</a>
-						<!-- /service box -->
-					</div>
-					<div>
-						<h4>KONGRE BURSU</h4>
-						<p>Çocuk Alerji ve İmmunoloji uzmanı ve yan dal asistanlarından 2022 Ulusal Kongremize bildiri göndermiş ve bildirisi bilimsel değerlendirme kurulu tarafından sözlü bildiri şeklinde kabul edilmiş olanlardan sınırlı sayıda kişiye kongre kayıt ve konaklama bursu verilmesi planlanmıştır.</p>
-						<p>Burstan yararlanmak isteyen ve aşağıdaki koşulları taşıyan üyelerimizin, 17.ÇAAAD Kongresi için, en geç 30 Mart 2023 tarihine kadar başvurmaları önemle rica olunur.</p>
-						<p>Başvuruların değerlendirilmeye alınabilmesi için ekte bulunan başvuru formunun doldurulup en geç 30 Mart’a kadar dernek caaad.org.tr adresine gönderilmesi gerekmektedir. (Derneğimizin web sitesi veya 2023 Kongre sitemizden de başvuru formuna ulaşılabilir.)</p>
-						<p>Saygılarımızla<br>ÇAAAD Yönetim Kurulu</p>
-					</div>
-					<div>
-						<h4>Kongre Katılım Bursu Başvuru Koşulları</h4>
-						<!-- Custom List -->
-						<ul class="custom no-margin" style="font-weight:normal;">
-							<li>Başvurmanın ön koşulu ÇAAAD üyesi olmaktır.</li>
-							<li>40 yaşın altındaki başvuru sahiplerine burs sağlanacaktır.</li>
-							<li>Burs kongreye sözel bildiri ile katılanlara verilecektir. Değerlendirme, CAAAD yönetim kurulunun belirleyeceği 3 kişilik “kongre bursu değerlendirme jürisi” tarafından yapılacaktır.</li>
-							<li>Sözlü bildirisi kabul edilenlere ve yazar listesinde ilk sırada yer alan veya bildiriyi sunan kişi öncelikli olacaktır.</li>
-							<li>Araştırma bildirileri ve retrospektif olmayan çalışmalar tercih edilecektir.</li>
-							<li>Birden çok sayıda bildiri ile katılanlara öncelik tanınacaktır.</li>
-							<li>Daha önce burs almamış olmak tercih sebebidir.</li>
-							<li>Burs verilecek kişilerin dağılımında, farklı kliniklerden kişilerin olmasına dikkat edilecektir.</li>
-							<li>Başvurular, kongre bildirimleri değerlendirme sonuçları belli olduktan sonra “kongre bursu başvuru formu” doldurularak yapılacaktır.</li>
-							<li>Kongre bursu kapsamında kongre katılım ücreti, konaklama (kongreye dahil en ucuz otel ve gerekirse iki kişi aynı odada kalacak şekilde) ve havaalanı transferi karşılanacaktır.</li>
-							<li>Başvuranlar, herhangi bir yerden kongreye katılım konusunda mali destek almadıklarına ilişkin yazıyı onaylayacaklardır.</li>
-						</ul>
-					</div>
-				</div>
+                <div class="section-heading">
+                    <h2 class="mt-5">BİLDİRİ GÖNDERİMİ</h2><!-- divider -->
+                    <div class="hr"></div>
+                </div>
+<!--                <div class="d-flex justify-content-center" style="padding-bottom:30px;">-->
+<!--                    <a href="#" target="_blank">-->
+<!--                        <div class="serviceBox col-lg-12 bg-color1" style="margin-top:0;">-->
+<!--                            <div class="service-content text-light">-->
+<!--                                <h4 class="title" style="font-size:23px; padding:0px; margin-bottom:0px; margin-top:0px;">Online Bildiri Gönderme Sistemi</h4>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </a>-->
+<!--                </div>-->
+                <div>
+                    <h4>Bildiri Gönderim Kuralları</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Kongreye bildiri başvuruları “Sözlü” ve “Poster” bildiri şeklinde iki grup altında yapılacaktır.</li>
+                        <li>Bildiri metinleri Türkçe olarak gönderilmelidir (Uluslararası katılımcılar için İngilizce). </li>
+                        <li>Sözlü bildirilere ayrı salon tahsis edilmiş olup çok sayıda sözlü bildirinin sunulması mümkün olacaktır. </li>
+                        <li>Bilimsel kurul, bildirileri sözlü veya poster bildiri olarak değiştirme konusunda karar verme hakkına sahiptir. </li>
+                        <li>Programda yer aldığı halde kongrede sunulmayan veya özeti zamanında teslim edilmeyen bildiriler bildiri özetleri kitabında yayınlanmayacaktır. Bildiri özetleri, Kongre Düzenleme Kurulu tarafından değerlendirilerek, kabul veya ret mektupları ve sunum biçimi (sözlü / poster) ...... tarihine kadar başvuru sahiplerine iletilecek ve aynı zamanda kongre resmi web sitesinde ilan edilecektir. </li>
+                        <li>Bildiri özetlerinin son gönderim tarihi ….2023 tarihi olup, bu tarihten sonra gönderilen bildiriler değerlendirmeye alınmayacaktır. Bildiriler kongreden sonra web sitesinde yayınlanacaktır. </li>
+                        <li>Kongre kayıt işleminin tamamlanması için kongre organizasyon firmasına kayıt talebinin bildirilmesi ve kayıt ücretinin belirtilen banka hesap numarasına yatırılması gerekmektedir </li>
+                        <li>Kayıt hakkında ayrıntılı bilgi için lütfen sitemizin “Kayıt” sayfasını ziyaret ediniz. Bildirisi kabul edilen katılımcıların ……….tarihine kadar kayıtlarını tamamlaması gerekmektedir. Kayıt işlemleri tamamlanmayan bildiriler, bilimsel programdan çıkarılacaktır. </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Yazarların Dikkat Etmesi Gereken Noktalar</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Bildirilerin kongre bildiri kitabında yayınlanması için kongre kaydının yapılmış olması zorunludur.</li>
+                        <li> Bildiriler sadece kongre web sitesi üzerinden online olarak kabul edilecektir ve hakem heyeti tarafından değerlendirilecektir. E-posta ile veya farklı şekilde gönderilen özetler değerlendirmeye alınmayacaktır.</li>
+                        <li> Özetler 300 kelimeyi geçmemelidir. Hatasız olarak yazılmalıdır (300 kelimenin üzeri, sistem tarafından kabul edilmemektedir.).</li>
+                        <li> Başlıklarda kısaltmalar kullanılmamalıdır. Başlık, yazar isimleri ve kurumların yazımları ile ilgili kurallar online sistemde verilmiştir.</li>
+                        <li> Yazarların isimleri, çalıştıkları yer, kurum ve il yazılırken uzun adresler ve unvan yazılmamalıdır.</li>
+                        <li> Özetler; Giriş, Materyal ve Metot, Bulgular ve Sonuç olarak belirlenen sıra ile yazılmalıdır.</li>
+                        <li> Uluslararası sistemdeki kısaltmalar dışındaki kısaltmalar kullanılmamalıdır.</li>
+                        <li> Kongremiz bünyesindeki tüm bildiriler Türkçe olarak sunulacaktır.</li>
+                        <li> Kabul veya ret mektuplarınız sistem üzerinde kayıtlı olan e-posta adresinize gönderilecektir. Ayrıca bildiri sonuçlarınızı sistem üzerinden de takip edebilirsiniz.</li>
+                    </ul>
+                    <p><b>DİKKAT:</b> Özetler, PDF Bildiri Kitabı'nda orijinal hali ile yerleştirilecek olup herhangi bir düzeltme yapılmayacaktır.</p>
+                    <p><b>NOT:</b> Gerekli tüm açıklamalar sistemde mevcuttur. Bildirinizle ilgili tüm değerlendirme sürecini e-posta ve parolanızla sistem üzerinden takip edebilirsiniz.</p>
+                </div>
+                <div>
+                    <h4>Tam Metin Nasıl Hazırlanmalıdır?</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Daha önceden hazırlamış olduğunuz dosyanızı (word formatında sadece yazı, resim veya tablo) bilgisayarınızda hazır bulundurunuz. </li>
+                        <li> Kabul edilen poster veya sözlü özet bildirilerinizi tam metin bildirisi olarak ayrı Word dosyası olarak hazırlayınız. </li>
+                        <li> Tam metin bildirileri maksimum 1300 kelime, 4 adet figür/resim, 4 adet tablo ve 20 kaynak ile sınırlıdır. </li>
+                        <li> Tam metin bildirilerinizi ... tarihine kadar Word formatında <b>lizozomal@soloevent.net</b> mail adresine gönderiniz. </li>
+                        <li> Tam metin bildiriler kongre sonrasında ayrıca ‘’Tam Metin Bildiri Kitabı’’ olarak yayınlanacaktır. Tam metin gönderimi zorunlu değildir. </li>
+                    </ul>
+                </div>
 
 			</section><!-- /section  -->
 		</div><!--/page-section-->
